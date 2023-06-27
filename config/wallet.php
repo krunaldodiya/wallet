ab0c6d5279245c6f7d8dc4dbc8020971fb3fc503
Krunal Dodiya <kunal.dodiya1@gmail.com>
2023-06-27 16:32:45 +0530

    done

--- config/wallet.php
+++ config/wallet.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
 return [
-    'user_model' => 'App\User',
+    'user_model' => 'App\Models\User',
     'wallet_model' => 'KD\Wallet\Models\Wallet',
     'transaction_model' => 'KD\Wallet\Models\WalletTransaction',
 ];
